2ad1c6e95b8ce50b935f27c2c03dc11c5936a247
ptreedev <166836879+ptreedev@users.noreply.github.com>
2024-12-14 14:34:03 +0000

    finished hero slice

--- pglisten-ai-demo/src/slices/Hero/index.tsx
+++ pglisten-ai-demo/src/slices/Hero/index.tsx
@@ -1,9 +1,14 @@
 import Bounded from "@/components/Bounded";
 import ButtonLink from "@/components/ButtonLink";
 import StarGrid from "@/components/StarGrid";
-import { Content } from "@prismicio/client";
+import { Content, isFilled } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import "@/app/globals.css";
 
 /**
  * Props for `Hero`.
@@ -18,15 +23,34 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="text-center"
     >
       <div className="relative">
         <StarGrid />
-        <PrismicRichText field={slice.primary.heading} />
-        <PrismicRichText field={slice.primary.body} />
-        <ButtonLink field={slice.primary.button_link}>
-          {slice.primary.button_label}
-        </ButtonLink>
-        <PrismicNextImage field={slice.primary.image} />
+        {isFilled.richText(slice.primary.heading) && (
+          <h1 className="text-balance text-center text-5xl font-medium md:text-7xl">
+            <PrismicText field={slice.primary.heading} />
+          </h1>
+        )}
+        {isFilled.richText(slice.primary.body) && (
+          <div className="mx-auto mt-6 max-w-md text-balance text-slate-300">
+            <PrismicRichText field={slice.primary.body} />
+          </div>
+        )}
+        {isFilled.link(slice.primary.button_link) && (
+          <ButtonLink className="mt-8" field={slice.primary.button_link}>
+            {slice.primary.button_label}
+          </ButtonLink>
+        )}
+        {isFilled.image(slice.primary.image) && (
+          <div className="glass-container mt-16 w-fit">
+            <div className="absolute inset-0 -z-10 bg-blue-500/30 blur-2xl filter" />
+            <PrismicNextImage
+              className="rounded-lg"
+              field={slice.primary.image}
+            />
+          </div>
+        )}
       </div>
     </Bounded>
   );
